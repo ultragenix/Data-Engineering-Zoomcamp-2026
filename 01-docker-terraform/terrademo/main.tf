71ba0b88f394673e567e0eaeefc27e2b974f2861
ultragenix <mangaman.mouny@gmail.com>
2026-01-26 21:15:29 +0400

    init terraform project and update gitignore for terraform

--- 01-docker-terraform/terrademo/main.tf
+++ 01-docker-terraform/terrademo/main.tf
@@ -3,15 +3,28 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "7.16.0"
     }
   }
 }
 
 provider "google" {
-  # Configuration options
   project     = "cohesive-folio-485508-e4"
   region      = "us-central1"
 }
 
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "cohesive-folio-485508-e4-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}

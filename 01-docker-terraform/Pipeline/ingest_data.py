0fc57c1d26228f2e345f963c8c308e7652864b9a
ultragenix <mangaman.mouny@gmail.com>
2026-01-26 16:02:31 +0400

    create and run docker compose and see how 3 container works together

--- 01-docker-terraform/Pipeline/ingest_data.py
+++ 01-docker-terraform/Pipeline/ingest_data.py
@@ -2,6 +2,8 @@
 import pandas as pd 
 from sqlalchemy import create_engine
 from tqdm import tqdm
+import click
+
 
 
 dtype = {
@@ -28,23 +30,22 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-
-def run():
-    pg_user = 'root'
-    pg_user = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-    chunksize=100000
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
 
     prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    engine = create_engine(f'postgresql://{pg_user}:{pg_user}@{pg_host}:{pg_port}/{pg_db}')
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
     df_iter = pd.read_csv(
         url,
